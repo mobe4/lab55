1ef2c4a7b56c02006b08204b980960773bb745cb
mobe4 <musebirhanu.teshale@miu.edu>
2024-04-07 20:56:39 -0500

    updated y

--- myADSDentalSurgeryDBScript.sql
+++ myADSDentalSurgeryDBScript.sql
@@ -1,1 +1,3 @@
-SELECT * FROM SurgeryLocation;
+INSERT INTO Appointment (date, time, dentist_Id, patient_Id, surgeryLocation_Id) VALUES
+('2024-04-15', '09:00:00', 1, 1, 1),
+('2024-04-16', '10:30:00', 2, 2, 2);

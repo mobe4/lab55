a381e537cbe8da92a5187bae45c8e2585eb493ba
mobe4 <musebirhanu.teshale@miu.edu>
2024-04-07 21:12:40 -0500

    jvdsb

--- myADSDentalSurgeryDBScript.sql
+++ myADSDentalSurgeryDBScript.sql
@@ -1,3 +1,1 @@
-INSERT INTO Appointment (date, time, dentist_Id, patient_Id, surgeryLocation_Id) VALUES
-('2024-04-15', '09:00:00', 1, 1, 1),
-('2024-04-16', '10:30:00', 2, 2, 2);
+SELECT * FROM Appointment;

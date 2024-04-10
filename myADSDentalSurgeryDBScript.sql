3769783e5160441fe4deeab8092f4362f461d64e
mobe4 <88381255+mobe4@users.noreply.github.com>
2024-04-09 20:20:20 -0500

    Update myADSDentalSurgeryDBScript.sql

--- myADSDentalSurgeryDBScript.sql
+++ myADSDentalSurgeryDBScript.sql
@@ -1,1 +1,14 @@
 SELECT * FROM Appointment;
+
+SELECT a.*, p.*
+FROM appointments a
+JOIN patients p ON a.patient_id = p.patient_id
+WHERE a.dentist_id = [Dentist_Id];
+
+SELECT *
+FROM appointments
+WHERE surgery_location_id = [Surgery_Location_Id];
+
+SELECT *
+FROM appointments
+WHERE patient_id = [Patient_Id] AND appointment_date = '[YYYY-MM-DD]';
